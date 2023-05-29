f893b04a7835aa5d65ee7f702fd64d2431966d23
claudiodmetrix <111041373+claudiodmetrix@users.noreply.github.com>
2023-05-28 22:38:28 -0400

    Se agrega javascript para encriptar y desencriptar

---
+++ script.js
@@ -0,0 +1,49 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar() {
+  const textoEncriptado = encriptar(textArea.value)
+  mensaje.value = textoEncriptado
+  textArea.value = "";
+  mensaje.style.backgroundImage = "none"
+}
+
+function btnDesncriptar() {
+  const textoEncriptado = desencriptar(textArea.value)
+  mensaje.value = textoEncriptado
+  textArea.value = "";
+}
+
+function encriptar(strinEncriptarParametro) {
+  let matrizCodigo = [{"e": "enter"}, {"i": "imes"}, {"a": "ai"}, {"o": "ober"}, {"u": "ufat"}];
+  strinEncriptarParametro = strinEncriptarParametro.toLowerCase()
+  matrizCodigo.forEach(objeto => {
+    const keys = Object.keys(objeto);
+    keys.forEach(key => {
+      if (strinEncriptarParametro.includes(key)) {
+        strinEncriptarParametro = strinEncriptarParametro.replaceAll(key, objeto[key]);
+      }
+    });
+  });
+  return strinEncriptarParametro;
+}
+
+function desencriptar(strinDesncriptarParametro) {
+  let matrizCodigo = [{"enter": "e"}, {"imes": "i"}, {"ai": "a"}, {"ober": "o"}, {"ufat": "u"}];
+  strinDesncriptarParametro = strinDesncriptarParametro.toLowerCase()
+  matrizCodigo.forEach(objeto => {
+    const keys = Object.keys(objeto);
+    keys.forEach(key => {
+      if (strinDesncriptarParametro.includes(key)) {
+        strinDesncriptarParametro = strinDesncriptarParametro.replaceAll(key, objeto[key]);
+      }
+    });
+  });
+  return strinDesncriptarParametro;
+}
